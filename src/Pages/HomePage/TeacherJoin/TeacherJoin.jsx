19529270216b99d44498a436c57a1257dfbea03d
afifa <2023afifa@gmail.com>
2024-03-12 21:13:46 +0600

    Instructor section updated

--- src/Pages/HomePage/TeacherJoin/TeacherJoin.jsx
+++ src/Pages/HomePage/TeacherJoin/TeacherJoin.jsx
@@ -2,14 +2,16 @@ import { Link } from "react-router-dom";
 
 const TeacherJoin = () => {
     return (
-        <div className="mb-20">
-            <div className="hero bg-slate-50">
-                <div className="hero-content flex-col lg:flex-row">
-                    <img src="https://i.ibb.co/rQdCVwb/Instructor.jpg" className="max-w-sm rounded-lg shadow-2xl" />
-                    <div>
-                        <h1 className="text-5xl font-bold">Become An Instructor</h1>
-                        <p className="py-6">Unlock the opportunity to share your expertise and passion with eager learners around the globe. Join our esteemed community of educators dedicated to shaping the next generation of professionals. As a teacher, you have the freedom to craft engaging courses in Web Development, Digital Marketing, Graphic Design, Data Science, and Language Learning. Showcase your skills, connect with a diverse audience, and make a lasting impact on the learning journey of others. Empower individuals to acquire new skills and achieve their goals. Join us on this educational adventure as we create a space for knowledge exchange and skill enhancement</p>
-                        <Link to="/request"><button className="btn bg-cyan-500 text-white normal-case">Start Teaching Today</button></Link>
+        <div className="my-20">
+            <div className="hero py-20" style={{ backgroundImage: 'url(https://i.ibb.co/fDDcW6w/instructor.jpg)' }}>
+                <div className="hero-overlay bg-opacity-30 max-w-3xl"></div>
+                <div className="hero-content text-center text-neutral-content">
+                    <div className="max-w-2xl">
+                        <h1 className="mb-5 text-5xl text-slate-100 font-bold">Become An Instructor</h1>
+                        <p className="mb-5 font-bold">Unlock the opportunity to share your expertise and passion with eager learners around the globe. Join our esteemed community of educators dedicated to shaping the next generation of professionals. As a teacher, you have the freedom to craft engaging courses in Web Development, Digital Marketing, Graphic Design, Data Science, and Language Learning. Showcase your skills, connect with a diverse audience, and make a lasting impact on the learning journey of others. Empower individuals to acquire new skills and achieve their goals. Join us on this educational adventure as we create a space for knowledge exchange and skill enhancement. </p>
+                        <Link to="/request">
+                            <button className="btn bg-sky-600 border-none text-white normal-case hover:bg-sky-700">Start Teaching Today</button>
+                        </Link>
                     </div>
                 </div>
             </div>
