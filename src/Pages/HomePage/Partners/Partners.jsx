724aee9d22bc42fa0758bec2f90b46deeef4a5bf
afifa <2023afifa@gmail.com>
2024-03-12 23:47:24 +0600

    Footer section updated

--- src/Pages/HomePage/Partners/Partners.jsx
+++ src/Pages/HomePage/Partners/Partners.jsx
@@ -2,7 +2,7 @@ const Partners = () => {
     return (
         <div className="my-20">
             <h3 className="text-center text-3xl font-bold mb-6">Our Partners</h3>
-            <div className="lg:mx-40 grid lg:grid-cols-5 items-center justify-center">
+            <div className="lg:mx-40 grid lg:grid-cols-5 items-center justify-center border-black border-2 p-5">
                 <div className="lg:mr-16">
                     <img className="w-40 h-14 mx-auto" src="https://i.ibb.co/yVzQMZg/partner1.png" alt="" />
                     {/* <p className="text-xs text-gray-500 text-center">Empowering innovation with Samsung - A leader in technology and innovation</p> */}
